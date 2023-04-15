c8a791a3a1275d9f484ff70b8fbd9459ab507500
PedroBastos <128756108+PedroBastos@users.noreply.github.com>
2023-04-15 14:53:07 -0300

    Questão 2 - Faça um programa que imprima todos os números ímpares entre 500 e 530. Utilize laço while e outras estruturas que julgar necessárias.

---
+++ QUESTAO02.java
@@ -0,0 +1,10 @@
+//Faça um programa que imprima todos os números ímpares entre 500 e 530.
+public class Main {
+    public static void main(String[] args) {
+        int num = 501;
+        while(num >=500 && num <531){
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
