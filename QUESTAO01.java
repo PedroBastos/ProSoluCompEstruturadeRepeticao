aad821b4a2c2292590f95961e5159f76002a1232
PedroBastos <128756108+PedroBastos@users.noreply.github.com>
2023-04-16 11:52:55 -0300

    Create QUESTAO01.java

---
+++ QUESTAO01.java
@@ -0,0 +1,15 @@
+/*Faça um programa que:
+- leia uma quantidade x de números que serão digitados. Por exemplo: 5
+- ler estes x números e concomitantemente realizar a soma dos valores sendo
+digitados. Utilize laço while e outras estruturas que julgar necessárias.
+- Exibir o resultado da soma dos x números digitados./*
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 100;
+        while(num >=100 && num <= 200){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
