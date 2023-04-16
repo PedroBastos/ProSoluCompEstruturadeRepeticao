9dff4d306939ac93c6d8f56eb126e99f2c2b462a
PedroBastos <128756108+PedroBastos@users.noreply.github.com>
2023-04-16 11:48:17 -0300

    Create QUESTAO03.java

---
+++ QUESTAO03.java
@@ -0,0 +1,11 @@
+/*Faça um programa que imprima em ordem decrescente todos os números entre 40 e 20. Utilize laço while e outras estruturas que julgar necessárias.*/
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 40;
+        while(num >=20){
+            System.out.println(num);
+            num--;
+        }
+    }
+}
